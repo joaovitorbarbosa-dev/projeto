23dc79b06a846e65c49ccbf3879861fc6e22b071
joaovitorbarbosa-dev <seu-email-do-github@example.com>
2025-09-21 18:59:45 -0300

    ádd store the function in a button

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function toggleMode(){
-    const html = document.documentElement
-    html.classList.toggle("light")
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-    //pegar a teg img
-    const img = document.querySelector("#profile img")
+  //pegar a teg img
+  const img = document.querySelector("#profile img");
 
-    //substituir a imagem
-    //se tiver light mode, adicionar a imagem light
-     if(html.classList.contains("light")) {
-        img.setAttribute("src", "assets/profile.png" )
-         }
-        
-    //se não tiver light mode, manter a imagem normal
-    else{
-        img.setAttribute("src", "assets/profile.png" )
+  //substituir a imagem
+  //se tiver light mode, adicionar a imagem light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "assets/profile.png");
+  }
 
-    }
-
-
-
-
+  //se não tiver light mode, manter a imagem normal
+  else {
+    img.setAttribute("src", "assets/profile.png");
+  }
 }
+
+// armazenar a função em um botão
+localStorage.setItem("mode", "dark");
+const cat = localStorage.getItem("mode");
+localStorager.removeItem("mode");
